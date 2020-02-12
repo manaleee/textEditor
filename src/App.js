b684a8cdf11ad2c95134fa551148b69fd65d1250
Manal Alkandari <manal_alkandri@hotmail.com>
2020-02-12 20:38:40 +0300

    mini project text editor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// ---------------------------------------------------------------------------
+
 const styles = {
   bold: { fontWeight: "bold" },
   italic: { fontStyle: "italic" },
@@ -9,27 +11,75 @@ const styles = {
 
 const stylings = ["bold", "italic", "underline"];
 
-const colors = ["yellow", "blue", "red", "black", "purple"];
+const colors = ["yellow", "blue", "red", "black", "purple", "pink"];
+
+// ---------------------------------------------------------------------------
 
 class App extends Component {
+  state = {
+    color: "blue",
+    bold: false,
+    italic: false,
+    underline: false
+  };
+
+  // ------------------------------------
+
+  // to choose color text
+  selectColor = color => {
+    this.setState({ color: color });
+  };
+
+  // ------------------------------------
+
+  // to choose new style text
+  selectStyle = style => {
+    const newSelectStyle = !this.state[style];
+    this.setState({ [style]: newSelectStyle });
+  };
+
+  // ------------------------------------
+
   render() {
     const stylingBoxes = stylings.map(style => (
-      <button className="btn btn-light" style={styles[style]} key={style}>
+      // <button className="btn btn-light" style={styles[style]} key={style}>
+      //   {style}
+      // </button>
+
+      <button
+        className="btn btn-light"
+        style={styles[style]}
+        key={style}
+        onClick={() => this.selectStyle(style)}
+        style={{ backgroundColor: style }}
+      >
         {style}
       </button>
     ));
+
+    // ------------------------------------
 
     const colorBoxes = colors.map(color => (
       <button
         style={{ backgroundColor: color, height: 30, width: 30 }}
         key={color}
+        onClick={() => this.selectColor(color)}
       />
     ));
+
+    // ------------------------------------
 
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
